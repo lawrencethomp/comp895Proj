560ecc01abfc50deeca047f5f5634d01d4099681
Qa Gemr <qagemr@Qas-MacBook-Pro-2.local>
2018-08-17 15:46:41 -0400

    added to github from macbook

--- node_api_m4a/app.js
+++ node_api_m4a/app.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+
 var ContactController = require('./ContactController');
 app.use('/contacts', ContactController);
 module.exports = app;
