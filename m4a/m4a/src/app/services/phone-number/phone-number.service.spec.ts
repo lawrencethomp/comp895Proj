c2181d53382e110460185432603e304e12f54fdb
El-Teezus <lawrencethomp@gmail.com>
2019-02-16 11:54:23 -0500

    Unit tests for phone number service

--- m4a/m4a/src/app/services/phone-number/phone-number.service.spec.ts
+++ m4a/m4a/src/app/services/phone-number/phone-number.service.spec.ts
@@ -1,20 +1,38 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { PhoneNumberService } from './phone-number.service';
 const phoneNumber = `6033692893`;
+const phoneNumberShort = `603369289`;
+const phoneNumberLong = `60336928931`;
+const phoneNumberLetter = `603369289a`;
+const phoneNumberSym = `603369289@`;
 describe('PhoneNumberService', () => {
+  let service: PhoneNumberService;
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [PhoneNumberService]
-    });
+    service = new PhoneNumberService();
   });
 
-  it('should be created', inject([PhoneNumberService], (service: PhoneNumberService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should make sure that a number has 10 digits', function() {
+    expect(service.parseNumber(phoneNumber)).toBe(true);
+  });
+  it('should make sure that a number cannot be under 10 digits', function() {
+    expect(service.parseNumber(phoneNumberShort)).toBe(`Enter a valid number.`);
+  });
+  it('should make sure that a number cannot be over 10 digits', function() {
+    expect(service.parseNumber(phoneNumberLong)).toBe(`Enter a valid number.`);
+  })
+  it('should only be able to have numbers', function() {
+    expect(service.parseNumber(phoneNumber)).toBe(true);
+  });
+  it('should not be able to have letters', function(){
+    expect(service.parseNumber(phoneNumberLetter)).toBe("Not a valid number.");
+  });
+  it(`should not be able to have symbols`, function(){
+    expect(service.parseNumber(phoneNumberSym)).toBe("Not a valid number.");
+  })
   it('should be able to format a number to (xxx) xxx-xxxx', function() {
-    expect(phoneNumber).toBe(`(603) 369-2893`);
+    expect(service.formatNumber(phoneNumber, 'paren')).toBe(`(603) 369-2893`);
   });
   it('should be able to format a number to xxx-xxx-xxxx', function() {
-    expect(phoneNumber).toBe(`603-369-893`);
+    expect(service.formatNumber(phoneNumber, 'dash')).toBe(`603-369-2893`);
   });
 });
--- m4a/m4a/src/app/services/phone-number/phone-number.service.ts
+++ m4a/m4a/src/app/services/phone-number/phone-number.service.ts
@@ -5,8 +5,30 @@ export class PhoneNumberService {
 
   constructor() {
 
-   }
-
-
-
+  }
+  /*
+    parseNumber - Makes sure that the number is a valid phone number
+  */
+    parseNumber(number: String) {
+      //check the string length
+      var numbers = /^[0-9]+$/;
+      if (number.match(numbers)) {
+      return (number.length === 10 ? true: "Enter a valid number." );
+  }
+  else {
+    return "Not a valid number."
+  }
+    }
+  /*
+    formatNumber - format number to be presented
+      in a better fashion when needed.
+  */
+    formatNumber(number: String, style: String) {
+        switch(style) {
+          case 'paren':
+            return `(${number.slice(0,3)}) ${number.slice(3,6)}-${number.slice(6,10)}`;
+          case 'dash':
+            return `${number.slice(0,3)}-${number.slice(3,6)}-${number.slice(6,10)}`;
+        };
+      } 
 }
