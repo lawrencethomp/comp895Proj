f5fc490c074042801ef75b1cc9189af566a27074
El-Teezus <lawrencethomp@gmail.com>
2019-07-09 11:30:53 -0400

    updated unit tests, switched to jest

--- node_api_m4a/test/contacts.test.js
+++ node_api_m4a/test/contacts.test.js
@@ -11,20 +11,15 @@ console.log(`felx first name ${felix.firstName}` )
 // TODO: add other unit tests to describe logic. 
     describe('Test Contacts', function() {
         it('Should Have a First Name', ()=> {
-            console.log(` got felix ${felix}`);
-            console.log(felix.firstName)
             assert.equal(felix.firstName, "Felix")
         })
         it('Should Have a Last Name', ()=> {
-            console.log(felix.lastName)
             assert.equal(felix.lastName, "Biederman")
         })
         it('Should Have a Phone Number', ()=> {
-            console.log(felix.phonenumber)
             assert.equal(felix.phonenumber, "540-833-9000");
         })
         it('Should Have an Email', ()=> {
-            console.log(felix.email)
             assert.equal(felix.email, "virgil@chapo.com");
         })
         
